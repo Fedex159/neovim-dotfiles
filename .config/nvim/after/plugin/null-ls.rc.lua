7218047554b6943d1676d9fc97149942715e9898
Fedex159 <fede.avelin.dev@gmailcom>
2022-09-11 13:33:48 -0300

    inject eslint_d actions

--- .config/nvim/after/plugin/null-ls.rc.lua
+++ .config/nvim/after/plugin/null-ls.rc.lua
@@ -14,6 +14,7 @@ null_ls.setup {
     null_ls.builtins.diagnostics.eslint_d.with({
       diagnostics_format = '[eslint] #{m}\n(#{c})'
     }),
+    null_ls.builtins.code_actions.eslint_d,
     null_ls.builtins.diagnostics.fish
   }
 }
