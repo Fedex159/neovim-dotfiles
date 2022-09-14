b76fbfadb06ceae66f92bece52fcb358bac616ec
Fedex159 <fede.avelin.dev@gmailcom>
2022-09-14 20:14:39 -0300

    fix colors bufferline

--- .config/nvim/after/plugin/bufferline.rc.lua
+++ .config/nvim/after/plugin/bufferline.rc.lua
@@ -1,14 +1,32 @@
 local status, bufferline = pcall(require, 'bufferline')
 if (not status) then return end
 
+local status2, colors = pcall(require, 'onedarkpro')
+if (not status2) then return end
+local c = colors.get_colors(vim.g.onedarkpro_theme)
+
 bufferline.setup {
   options = {
     mode = 'tabs',
     separator_style = 'slant',
     always_show_bufferline = false,
-    show_buffer_close_icons = false,
+    show_buffer_close_icons = true,
     show_close_icon = false,
-    color_icons = true
+    color_icons = true,
+    diagnostics = 'nvim_lsp',
+    indicator = {
+      style = 'icon'
+    },
+    numbers = 'ordinal'
+  },
+  highlights = {
+    separator = {
+      fg = c.bg,
+      bg = c.bg,
+    },
+    separator_selected = {
+      fg = c.green,
+    },
   },
 }
 
