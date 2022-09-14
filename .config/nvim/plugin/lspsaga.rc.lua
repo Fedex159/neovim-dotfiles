270fc826ba1626e67a42ddf1ae5071df05166809
federico avelin <favelin@litebox.ai>
2022-09-14 10:34:36 -0300

    change preview definition for peek definition in lspsaga settings

--- .config/nvim/plugin/lspsaga.rc.lua
+++ .config/nvim/plugin/lspsaga.rc.lua
@@ -9,7 +9,7 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<cr>', opts)
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<cr>', opts)
 vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<cr>', opts)
-vim.keymap.set('n', 'gp', '<Cmd>Lspsaga preview_definition<cr>', opts)
+vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<cr>', opts)
 vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<cr>', opts)
 vim.keymap.set('n', 'ga', '<Cmd>Lspsaga code_action<cr>', opts)
 vim.keymap.set('n', 'gt', '<Cmd>Lspsaga open_floaterm<cr>', opts)
