118ab10f4133a1737c9167d5319fa4727e0158a6
Fedex159 <fede.avelin.dev@gmailcom>
2022-09-15 20:36:16 -0300

    fix colors tabs

--- .config/nvim/after/plugin/bufferline.rc.lua
+++ .config/nvim/after/plugin/bufferline.rc.lua
@@ -5,28 +5,39 @@ local status2, colors = pcall(require, 'onedarkpro')
 if (not status2) then return end
 local c = colors.get_colors(vim.g.onedarkpro_theme)
 
+
 bufferline.setup {
   options = {
     mode = 'tabs',
     separator_style = 'slant',
     always_show_bufferline = false,
-    show_buffer_close_icons = true,
+    show_buffer_close_icons = false,
     show_close_icon = false,
     color_icons = true,
     diagnostics = 'nvim_lsp',
     indicator = {
       style = 'icon'
     },
-    numbers = 'ordinal'
   },
   highlights = {
     separator = {
-      fg = c.bg,
-      bg = c.bg,
+      fg = '#0D0E10',
+      bg = '#1C1F24',
     },
     separator_selected = {
-      fg = c.green,
+      fg = '#0D0E10',
     },
+    background = {
+      fg = '#657b83',
+      bg = '#1C1F24',
+    },
+    buffer_selected = {
+      fg = c.blue,
+      bold = true,
+    },
+    fill = {
+      bg = '#0D0E10'
+    }
   },
 }
 
