89c3642f9710ad2f2af9ea463d2213032172e864
Fedex159 <fede.avelin.dev@gmailcom>
2022-09-15 19:38:21 -0300

    config Gitsings

--- .config/nvim/after/plugin/gitsigns.rc.lua
+++ .config/nvim/after/plugin/gitsigns.rc.lua
@@ -1,4 +1,15 @@
 local status, gitsigns = pcall(require, 'gitsigns')
 if (not status) then return end
 
-gitsigns.setup {}
+gitsigns.setup {
+  current_line_blame = true,
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    delay = 100,
+    ignore_whitespace = false,
+  },
+  current_line_blame_formatter = '<author>, <author_time:%R> · <summary>'
+}
+
+vim.keymap.set('n', '<leader>gf', '<cmd>Gitsigns diffthis<cr>')
