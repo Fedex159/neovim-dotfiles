e2bd49b1a51db24c0ef3a8caed159d339f1331a5
Fedex159 <fede.avelin.dev@gmailcom>
2022-09-16 19:00:16 -0300

    add maps for move lines up and down

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -34,3 +34,7 @@ keymap.set('n', '<A-S-left>', '<C-w><')
 keymap.set('n', '<A-S-right>', '<C-w>>')
 keymap.set('n', '<A-S-up>', '<C-w>+')
 keymap.set('n', '<A-S-down>', '<C-w>-')
+
+-- Move lines
+vim.api.nvim_set_keymap('v', '<S-down>', ":m '>+1<CR>gv=gv", { noremap = true })
+vim.api.nvim_set_keymap('v', '<S-up>', ":m '<-2<CR>gv=gv", { noremap = true })
