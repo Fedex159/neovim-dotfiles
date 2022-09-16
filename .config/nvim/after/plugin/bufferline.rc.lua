f7c5e46d3aa9465acf1d9adbbe537d9d6a551b37
federico avelin <favelin@litebox.ai>
2022-09-16 10:33:25 -0300

    show always bufferline

--- .config/nvim/after/plugin/bufferline.rc.lua
+++ .config/nvim/after/plugin/bufferline.rc.lua
@@ -10,7 +10,7 @@ bufferline.setup {
   options = {
     mode = 'tabs',
     separator_style = 'slant',
-    always_show_bufferline = false,
+    always_show_bufferline = true,
     show_buffer_close_icons = false,
     show_close_icon = false,
     color_icons = true,
