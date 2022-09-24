619aa1dc597a5ba373681727987f36693fdd5200
Fedex159 <fede.avelin.dev@gmailcom>
2022-09-24 20:58:14 -0300

    add git diff to lualine

--- .config/nvim/after/plugin/lualine.rc.lua
+++ .config/nvim/after/plugin/lualine.rc.lua
@@ -1,6 +1,20 @@
 local status, lualine = pcall(require, 'lualine')
 if (not status) then return end
 
+local colors = {
+  bg       = '#202328',
+  fg       = '#bbc2cf',
+  yellow   = '#ECBE7B',
+  cyan     = '#008080',
+  darkblue = '#081633',
+  green    = '#98be65',
+  orange   = '#FF8800',
+  violet   = '#a9a1e1',
+  magenta  = '#c678dd',
+  blue     = '#51afef',
+  red      = '#ec5f67',
+}
+
 lualine.setup {
   options = {
     icons_enable = true,
@@ -12,11 +26,22 @@ lualine.setup {
   sections = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch' },
-    lualine_c = { {
-      'filename',
-      file_status = true, --display file status
-      path = 0 -- 0 = filename
-    } },
+    lualine_c = {
+      {
+        'filename',
+        file_status = true, --display file status
+        path = 0 -- 0 = filename
+      },
+      {
+        'diff',
+        symbols = { added = ' ', modified = '柳 ', removed = ' ' },
+        diff_color = {
+          added = { fg = colors.green },
+          modified = { fg = colors.orange },
+          removed = { fg = colors.red },
+        },
+      }
+    },
     lualine_x = {
       { 'diagnostics', sources = { "nvim_diagnostic" }, symbols = { error = ' ', warn = ' ', info = ' ',
         hint = ' ' } },
