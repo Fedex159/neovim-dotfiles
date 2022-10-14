9672105d894352d4de22b5f83dc513c3c5202acb
Fedex159 <fede.avelin.dev@gmailcom>
2022-10-13 21:33:07 -0300

    config highlights for gitsigns

--- .config/nvim/after/plugin/gitsigns.rc.lua
+++ .config/nvim/after/plugin/gitsigns.rc.lua
@@ -1,6 +1,11 @@
 local status, gitsigns = pcall(require, 'gitsigns')
 if (not status) then return end
 
+local diffAdded = '#98be65'
+local diffDelete = '#ec5f67'
+local diffChanged = '#ECBE7B'
+local currentLineBlame = '#5C6773'
+
 gitsigns.setup {
   current_line_blame = true,
   current_line_blame_opts = {
@@ -11,5 +16,10 @@ gitsigns.setup {
   },
   current_line_blame_formatter = '<author>, <author_time:%R> · <summary>'
 }
+
+vim.api.nvim_set_hl(0, 'GitSignsAdd', { fg = diffAdded })
+vim.api.nvim_set_hl(0, 'GitSignsDelete', { fg = diffDelete })
+vim.api.nvim_set_hl(0, 'GitSignsChange', { fg = diffChanged })
+vim.api.nvim_set_hl(0, 'GitSignsCurrentLineBlame', { fg = currentLineBlame })
 
 vim.keymap.set('n', '<leader>gf', '<cmd>Gitsigns diffthis<cr>')
