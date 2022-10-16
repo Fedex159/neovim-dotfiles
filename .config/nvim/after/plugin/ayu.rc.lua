30dcfb4a9749a7ac2a614d7d996b826dfaa7b47b
Fedex159 <fede.avelin.dev@gmail.com>
2022-10-16 20:20:57 -0300

    config transparency background

--- .config/nvim/after/plugin/ayu.rc.lua
+++ .config/nvim/after/plugin/ayu.rc.lua
@@ -6,3 +6,11 @@ ayu.setup({
 })
 
 ayu.colorscheme()
+
+-- Original: #262335
+-- New: #1F2430
+vim.cmd [[highlight Normal guibg=none]]
+vim.cmd [[highlight SignColumn guibg=none]]
+vim.cmd [[highlight CursorLine guibg=none gui=bold]]
+vim.cmd [[highlight CursorLineNr guibg=none gui=bold]]
+vim.cmd [[highlight LineNr guifg=#5CCFE6]]
