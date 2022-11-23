add1ccd1c1d9a883ba89cc5995133b3e1f3ed39b
Fedex159 <fede.avelin.dev@gmail.com>
2022-11-23 20:44:21 -0300

    update config gitsigns

--- .config/nvim/after/plugin/gitsigns.rc.lua
+++ .config/nvim/after/plugin/gitsigns.rc.lua
@@ -7,6 +7,14 @@ local diffChanged = '#ECBE7B'
 local currentLineBlame = '#5C6773'
 
 gitsigns.setup {
+  signs = {
+    add          = { hl = 'GitSignsAdd', text = '│', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
+    change       = { hl = 'GitSignsChange', text = '│', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+    delete       = { hl = 'GitSignsDelete', text = '_', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+    topdelete    = { hl = 'GitSignsDelete', text = '‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+    changedelete = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+    untracked    = { hl = 'GitSignsAdd', text = '│', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
+  },
   current_line_blame = true,
   current_line_blame_opts = {
     virt_text = true,
--- .config/nvim/after/plugin/null-ls.rc.lua
+++ .config/nvim/after/plugin/null-ls.rc.lua
@@ -15,7 +15,6 @@ end
 null_ls.setup {
   sources = {
     null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.code_actions.gitsigns,
     null_ls.builtins.formatting.eslint_d,
     null_ls.builtins.diagnostics.eslint_d.with({
       diagnostics_format = '[eslint] #{m}\n(#{c})'
