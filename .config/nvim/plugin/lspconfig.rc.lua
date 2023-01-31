23a3b264e8c881682463ef1171b98cb647bc36ef
Fedex159 <fede.avelin.dev@gmail.com>
2023-01-31 19:17:09 -0300

    fix: disable third party sumneko

--- .config/nvim/plugin/lspconfig.rc.lua
+++ .config/nvim/plugin/lspconfig.rc.lua
@@ -89,7 +89,8 @@ nvim_lsp.sumneko_lua.setup {
 
       workspace = {
         -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true)
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
       }
     }
   }
