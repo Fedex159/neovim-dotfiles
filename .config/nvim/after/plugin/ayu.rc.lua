e704e0d8ba94a7227b356e171f954f5c9582b3e6
Fedex159 <fede.avelin.dev@gmail.com>
2023-02-07 20:06:34 -0300

    feat: config highlights for lspsaga

--- .config/nvim/after/plugin/ayu.rc.lua
+++ .config/nvim/after/plugin/ayu.rc.lua
@@ -7,6 +7,13 @@ ayu.setup({
 
 ayu.colorscheme()
 
+local colors = {
+  red = '#e95678',
+  yellow = '#f7bb3b',
+  cyan = '#36d0e0',
+  purple = '#CBA6F7',
+}
+
 -- Original: #262335
 -- New: #1F2430
 vim.cmd [[highlight Normal guibg=none]]
@@ -14,3 +21,11 @@ vim.cmd [[highlight SignColumn guibg=none]]
 vim.cmd [[highlight CursorLine guibg=none gui=bold]]
 vim.cmd [[highlight CursorLineNr guibg=none gui=bold]]
 vim.cmd [[highlight LineNr guifg=#5CCFE6]]
+
+vim.cmd(string.format([[ hi CodeActionNumber guibg=none guifg=%s ]], colors.yellow))
+vim.cmd(string.format([[ hi OutlineIndent guibg=none guifg=%s ]], colors.red))
+vim.cmd(string.format([[ hi SagaBorder guibg=none guifg=%s ]], colors.cyan))
+
+vim.cmd(string.format([[ hi FinderIcon guibg=none guifg=%s ]], colors.cyan))
+vim.cmd(string.format([[ hi FinderType guibg=none guifg=%s ]], colors.purple))
+vim.cmd(string.format([[ hi FinderSelection guibg=none guifg=%s gui=bold ]], colors.cyan))
