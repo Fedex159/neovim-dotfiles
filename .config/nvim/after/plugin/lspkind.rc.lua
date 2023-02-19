8b7a4c619ae63a1a71c8f34ed4f80282c17498c3
Fedex159 <fede.avelin.dev@gmail.com>
2023-02-19 12:48:01 -0300

    fix: add import validation lspkind

--- .config/nvim/after/plugin/lspkind.rc.lua
+++ .config/nvim/after/plugin/lspkind.rc.lua
@@ -1,4 +1,7 @@
-require('lspkind').init({
+local status, lspkind = pcall(require, 'lspkind')
+if (not status) then return end
+
+lspkind.init({
   -- DEPRECATED (use mode instead): enables text annotations
   --
   -- default: true
@@ -8,14 +11,12 @@ require('lspkind').init({
   -- default: symbol
   -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
   mode = 'symbol_text',
-
   -- default symbol map
   -- can be either 'default' (requires nerd-fonts font) or
   -- 'codicons' for codicon preset (requires vscode-codicons font)
   --
   -- default: 'default'
   preset = 'codicons',
-
   -- override preset symbols
   --
   -- default: {}
