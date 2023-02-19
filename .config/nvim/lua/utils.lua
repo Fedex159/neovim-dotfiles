0bd436af1b38c12b2dbf0781ec143222e0206594
Fedex159 <fede.avelin.dev@gmail.com>
2023-02-19 12:49:09 -0300

    feat: create util to update highlights easy

---
+++ .config/nvim/lua/utils.lua
@@ -0,0 +1,17 @@
+local function highlight(group, keys, args)
+  local result = table.concat(keys, '=%s ')
+  local resultLength = string.len(result)
+  local lastChar = string.sub(result, resultLength - 2)
+
+  if (lastChar ~= '=%s') then
+    result = result .. '=%s'
+  end
+
+  local color = string.format(result, table.unpack(args))
+  local cmd = string.format('highlight %s %s', group, color)
+  vim.cmd(cmd)
+end
+
+return {
+  highlight = highlight
+}
