b88a9d5bcadf611981053fa673d30907b179a752
Fedex159 <fede.avelin.dev@gmail.com>
2023-05-01 13:03:18 -0300

    feat: add description to reference map

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,7 +2,8 @@ return {
   "neovim/nvim-lspconfig",
   init = function()
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    keys[#keys + 1] = { "gr", "<cmd>lua require('telescope.builtin').lsp_references({ show_line=false })<cr>" }
+    keys[#keys + 1] =
+      { "gr", "<cmd>lua require('telescope.builtin').lsp_references({ show_line=false })<cr>", desc = "References" }
   end,
   dependencies = { "jose-elias-alvarez/typescript.nvim" },
   opts = {
