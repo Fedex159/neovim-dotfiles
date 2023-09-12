f21a622bece2195896d5f85a0beca5e0c6d4a6d4
Fedex159 <fede.avelin.dev@gmail.com>
2023-09-12 20:16:20 -0300

    fix: disable auto close tag in slash

--- .config/nvim/lua/plugins/ts-autotag.lua
+++ .config/nvim/lua/plugins/ts-autotag.lua
@@ -2,6 +2,8 @@ return {
   "windwp/nvim-ts-autotag",
   event = { "BufReadPre", "BufNewFile" },
   config = function()
-    require("nvim-ts-autotag").setup({})
+    require("nvim-ts-autotag").setup({
+      enable_close_on_slash = false,
+    })
   end,
 }
