a266aa0afcdf55b16e11ac205d36c76524caddbd
Fedex159 <fede.avelin.dev@gmail.com>
2024-05-21 19:27:02 -0300

    fix: update modified icon

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -28,7 +28,7 @@ return {
           },
           {
             "diff",
-            symbols = { added = " ", modified = "柳 ", removed = " " },
+            symbols = { added = " ", modified = " ", removed = " " },
             diff_color = {
               added = { fg = colors.green },
               modified = { fg = colors.orange },
