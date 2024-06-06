846e24cdcb992054752078568ffa632e56323d47
Fedex159 <fede.avelin.dev@gmail.com>
2024-06-06 19:25:58 -0300

    refactor: update config mason

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,6 +1,8 @@
 return {
   "williamboman/mason.nvim",
-  opts = function(_, opts)
-    table.insert(opts.ensure_installed, "prettierd")
-  end,
+  opts = {
+    ensure_installed = {
+      "prettierd",
+    },
+  },
 }
