0503bf66f73ad6aa7463947ccf07e773ef19d26e
Fedex159 <fede.avelin.dev@gmail.com>
2024-06-13 19:48:00 -0300

    refactor: remove config for nvim_lsp and use the config by lazyvim

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -3,50 +3,12 @@ return {
   opts = {
     inlay_hints = { enabled = false },
     servers = {
-      tsserver = {
-        enabled = false,
-      },
       vtsls = {
         handlers = {
-          ---@diagnostic disable-next-line: redundant-parameter
           ["textDocument/definition"] = function(err, result, ...)
-            result = vim.tbl_islist(result) and result[1] or result
+            result = vim.islist(result) and result[1] or result
             vim.lsp.handlers["textDocument/definition"](err, result, ...)
           end,
-        },
-        filetypes = {
-          "javascript",
-          "javascriptreact",
-          "javascript.jsx",
-          "typescript",
-          "typescriptreact",
-          "typescript.tsx",
-        },
-        settings = {
-          complete_function_calls = true,
-          vtsls = {
-            enableMoveToFileCodeAction = true,
-            autoUseWorkspaceTsdk = true,
-            experimental = {
-              completion = {
-                enableServerSideFuzzyMatch = true,
-              },
-            },
-          },
-          typescript = {
-            updateImportsOnFileMove = { enabled = "always" },
-            suggest = {
-              completeFunctionCalls = true,
-            },
-            inlayHints = {
-              enumMemberValues = { enabled = true },
-              functionLikeReturnTypes = { enabled = true },
-              parameterNames = { enabled = "literals" },
-              parameterTypes = { enabled = true },
-              propertyDeclarationTypes = { enabled = true },
-              variableTypes = { enabled = false },
-            },
-          },
         },
         keys = {
           {
@@ -92,17 +54,6 @@ return {
           },
         },
       },
-    },
-    setup = {
-      tsserver = function()
-        -- disable tsserver
-        return true
-      end,
-      vtsls = function(_, opts)
-        -- copy typescript settings to javascript
-        opts.settings.javascript =
-          vim.tbl_deep_extend("force", {}, opts.settings.typescript, opts.settings.javascript or {})
-      end,
     },
   },
 }
