93887ebe5595846fe209f67adc7669494f34ed30
Fedex159 <fede.avelin.dev@gmail.com>
2024-06-13 19:46:59 -0300

    refactor: disable signs staged

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -10,6 +10,7 @@ return {
       changedelete = { text = "~" },
       untracked = { text = "│" },
     },
+    signs_staged_enable = false,
     current_line_blame = true,
     current_line_blame_opts = {
       virt_text = true,
