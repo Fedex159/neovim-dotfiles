c3ea5c9758a6eabcd9c8906fef340d6c1cc99596
Fedex159 <fede.avelin.dev@gmail.com>
2024-06-28 18:27:49 -0300

    fix: add mini.pairs config and fix errors with strings

---
+++ .config/nvim/lua/plugins/mini-pairs.lua
@@ -0,0 +1,11 @@
+return {
+  "echasnovski/mini.pairs",
+  event = "VeryLazy",
+  opts = {
+    modes = { insert = true, command = true, terminal = false },
+    skip_next = false,
+    skip_ts = {},
+    skip_unbalanced = false,
+    markdown = true,
+  },
+}
