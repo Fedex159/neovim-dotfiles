853386763edcfd8ae39b1cb2ffa9bd3c021c355c
federico avelin <favelin@litebox.ai>
2024-07-24 09:12:45 -0300

    fix: update conform config

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -2,10 +2,10 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      typescript = { { "prettierd", "prettier" } },
-      typescriptreact = { { "prettierd", "prettier" } },
-      javascript = { { "prettierd", "prettier" } },
-      javascriptreact = { { "prettierd", "prettier" } },
+      typescript = { "prettier", "prettierd" },
+      typescriptreact = { "prettier", "prettierd" },
+      javascript = { "prettier", "prettierd" },
+      javascriptreact = { "prettier", "prettierd" },
 
       ["*"] = { "codespell" },
       -- Use the "_" filetype to run formatters on filetypes that don't
