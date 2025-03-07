6c3a8462d2e3aa23cbd1f31a305d95349578bf62
Federico Avelin <fede.avelin.dev@gmail.com>
2025-03-07 20:32:23 -0300

    refactor: add recording status to lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -19,7 +19,21 @@ return {
       },
       sections = {
         lualine_a = { "mode" },
-        lualine_b = { "branch" },
+        lualine_b = {
+          "branch",
+          {
+            "macro",
+            fmt = function()
+              local reg = vim.fn.reg_recording()
+              if reg ~= "" then
+                return "Recording @" .. reg
+              end
+              return nil
+            end,
+            color = { fg = "#ff9e64" },
+            draw_empty = false,
+          },
+        },
         lualine_c = {
           {
             "filename",
