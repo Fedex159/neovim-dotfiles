e00d1c3347a69e7174a5e4609885fbb515e9d0df
Federico Avelin <fede.avelin.dev@gmail.com>
2025-04-23 20:10:24 -0300

    refactor: replace fzf for snacks.picker

--- .config/nvim/lua/plugins/fzf-lua.lua
+++
@@ -1,9 +0,0 @@
-local config = require("fzf-lua.config")
-local actions = require("fzf-lua.actions")
-
-config.defaults.actions.files["ctrl-w"] = actions.file_tabedit
-
-return {
-  "ibhagwan/fzf-lua",
-  cmd = "FzfLua",
-}
