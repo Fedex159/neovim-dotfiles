27099ec5edd014b33da56b1d3ebc6b3884dba574
Federico Avelin <fede.avelin.dev@gmail.com>
2025-09-20 12:31:55 -0300

    refactor: remove fixed version of catppuccin theme and override color to use previous

--- .config/nvim/lua/plugins/catppucin.lua
+++ .config/nvim/lua/plugins/catppucin.lua
@@ -2,9 +2,19 @@ return {
   "catppuccin/nvim",
   lazy = true,
   name = "catppuccin",
-  commit = "e762a47478377612e59f01aa51db961f605c9aa4",
+  opts = function(_, opts)
+    local module = require("catppuccin.groups.integrations.bufferline")
+    if module then
+      module.get = module.get_theme
+    end
+    return opts
+  end,
   config = function()
     require("catppuccin").setup({
+      float = {
+        transparent = false,
+        solid = false,
+      },
       kitty = false,
       color_overrides = {
         mocha = {
@@ -15,12 +25,50 @@ return {
       },
       highlight_overrides = {
         mocha = function(cp)
+          local O = require("catppuccin").options
+
           return {
             ["@tag"] = { fg = cp.lavender },
             ["@tag.attribute.tsx"] = { fg = cp.mauve, style = { "italic" } },
             ["@tag.attribute.javascript"] = { fg = cp.mauve, style = { "italic" } },
             ["@_jsx_attribute.javascript"] = { fg = cp.mauve, style = { "italic" } },
             ["@tag.builtin"] = { fg = cp.mauve },
+            -- Override new highlights
+            ["@variable.member"] = { fg = cp.lavender }, -- For fields.
+            ["@module"] = { fg = cp.lavender, style = O.styles.miscs or { "italic" } }, -- For identifiers referring to modules and namespaces.
+            ["@string.special.url"] = { fg = cp.rosewater, style = { "italic", "underline" } }, -- urls, links and emails
+            ["@type.builtin"] = { fg = cp.yellow, style = O.styles.properties or { "italic" } }, -- For builtin types.
+            ["@property"] = { fg = cp.lavender, style = O.styles.properties or {} }, -- Same as TSField.
+            ["@constructor"] = { fg = cp.sapphire }, -- For constructor calls and definitions: = { } in Lua, and Java constructors.
+            ["@keyword.operator"] = { link = "Operator" }, -- For new keyword operator
+            ["@keyword.export"] = { fg = cp.sky, style = O.styles.keywords },
+            ["@markup.strong"] = { fg = cp.maroon, style = { "bold" } }, -- bold
+            ["@markup.italic"] = { fg = cp.maroon, style = { "italic" } }, -- italic
+            ["@markup.heading"] = { fg = cp.blue, style = { "bold" } }, -- titles like: # Example
+            ["@markup.quote"] = { fg = cp.maroon, style = { "bold" } }, -- block quotes
+            ["@markup.link"] = { link = "Tag" }, -- text references, footnotes, citations, etc.
+            ["@markup.link.label"] = { link = "Label" }, -- link, reference descriptions
+            ["@markup.link.url"] = { fg = cp.rosewater, style = { "italic", "underline" } }, -- urls, links and emails
+            ["@markup.raw"] = { fg = cp.teal }, -- used for inline code in markdown and for doc in python (""")
+            ["@markup.list"] = { link = "Special" },
+            ["@tag.attribute"] = { fg = cp.teal, style = O.styles.miscs or { "italic" } }, -- Tags like html tag names.
+            ["@tag.delimiter"] = { fg = cp.sky }, -- Tag delimiter like < > /
+            ["@property.css"] = { fg = cp.lavender },
+            ["@property.id.css"] = { fg = cp.blue },
+            ["@type.tag.css"] = { fg = cp.mauve },
+            ["@string.plain.css"] = { fg = cp.peach },
+            ["@constructor.lua"] = { fg = cp.flamingo }, -- For constructor calls and definitions: = { } in Lua.
+            -- typescript
+            ["@property.typescript"] = { fg = cp.lavender, style = O.styles.properties or {} },
+            ["@constructor.typescript"] = { fg = cp.lavender },
+            -- TSX (Typescript React)
+            ["@constructor.tsx"] = { fg = cp.lavender },
+            ["@type.builtin.c"] = { fg = cp.yellow, style = {} },
+            ["@type.builtin.cpp"] = { fg = cp.yellow, style = {} },
+            -- JSON
+            ["@property.json"] = { fg = cp.lavender }, -- For fields.
+            -- Enum
+            ["@lsp.type.enumMember"] = { link = "@constant" },
           }
         end,
       },
