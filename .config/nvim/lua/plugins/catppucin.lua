e3cceac2aa8e1e75e6981182c9572b5df51a0b0a
Federico Avelin <fede.avelin.dev@gmail.com>
2025-10-28 10:13:42 -0300

    refactor: remove partial fix for bufferline with catppucin theme

--- .config/nvim/lua/plugins/catppucin.lua
+++ .config/nvim/lua/plugins/catppucin.lua
@@ -2,13 +2,6 @@ return {
   "catppuccin/nvim",
   lazy = true,
   name = "catppuccin",
-  opts = function(_, opts)
-    local module = require("catppuccin.groups.integrations.bufferline")
-    if module then
-      module.get = module.get_theme
-    end
-    return opts
-  end,
   config = function()
     require("catppuccin").setup({
       float = {
